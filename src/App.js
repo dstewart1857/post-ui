57175d3232e1ab4106fb02c526e238f5294f7d7c
Doug Stewart <Doug.Stewart@Henryschein.com>
2022-05-22 18:14:00 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,99 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [userInput, setUserInput] = React.useState('');
-  const [answer, setAnswer] = React.useState('8');
+  const [userInput, setUserInput] = React.useState('<Enter Name Here>');
+  const [answer, setAnswer] = React.useState('Hi!');
   const [getAnswer, setGetAnswer] = React.useState(false);
+  const [addTests, setAddTests] = React.useState(false);
 
   React.useEffect(() => {
-    document.title = 'Magic 8 Ball';
+    document.title = 'Make a Request';
   });
 
   const ask = () => {
     if (userInput) {
-      setUserInput('');
+      //setUserInput('');
       setGetAnswer(true);
     }
   };
 
+    const add = () => {
+        if (userInput) {
+            setAddTests(true);
+        }
+    };
+
+    React.useEffect(() => {
+        var submitTests = async () => {
+            // Refactor code Start
+
+            var response = null;
+            try {
+                setAnswer('Waiting...');
+
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: userInput
+                };
+
+                response = await fetch('https://localhost:7022/ReportCard/submitTests', requestOptions);
+            } catch (err) {
+                console.log(err);
+                setAnswer('Help! I\'m broken - failed to get a response from the desired endpoint.');
+            }
+
+            if (response != null) {
+                if (response.status >= 200 && response.status <= 299) {
+                    const responseText = await response.text();
+                    console.log(responseText);
+                    setAnswer(responseText);
+                } else {
+                    console.log(response.status, response.statusText);
+                    setAnswer('Help! I\'m broken - received invalid response code.');
+                }
+            }
+            // Refactor code End
+        };
+
+        if (addTests) {
+            submitTests();
+            //setUserInput('');
+            setAddTests(false);
+        };
+
+    }, [addTests]);
+
+
   React.useEffect(() => {
-    const acquireAnswer = async () => {
+    var acquireAnswer = async () => {
       // Refactor code Start
 
-      setAnswer("Help! I'm broken");
+        var response = null;
+        try {
+            setAnswer('Waiting...');
+            response = await fetch('https://localhost:7022/ReportCard/getTests');
+        } catch (err) {
+            console.log(err);
+            setAnswer('Help! I\'m broken - failed to get a response from the desired endpoint.');
+        }
 
+        if (response != null) {
+            if (response.status >= 200 && response.status <= 299) {
+                const responseText = await response.text();
+                console.log(responseText);
+                setAnswer(responseText);
+            } else {
+                console.log(response.status, response.statusText);
+                setAnswer('Help! I\'m broken - received invalid response code.');
+            }
+        }
       // Refactor code End
     };
 
     if (getAnswer) {
-      acquireAnswer();
+        acquireAnswer();
+        setUserInput('');
       setGetAnswer(false);
     };
 
@@ -42,11 +109,14 @@ function App() {
           onChange={(event) => { 
             setUserInput(event.target.value)
             setGetAnswer(false)
-            setAnswer('8')
+            setAnswer('Hi!')
           }}
         />
+        <button onClick={add}>
+          Add Tests to Collection!
+        </button>
         <button onClick={ask}>
-          Ask the Magic Eight Ball!
+          List Tests in Collection!
         </button>
       </div>
 
@@ -54,10 +124,10 @@ function App() {
         <div className="ball-black-outer">
           <div className="ball-white-inner">
             <div>
-              {(answer !== '8') ?
+              {(answer !== 'Hi!') ?
                 answer
                 :
-                <div className='eight'>8</div>
+                <div className='eight'>Hi!</div>
               }
             </div>
           </div>
